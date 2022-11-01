e14f05a52d816703dd9f790d70439f4eec645fb5
muvseea <muvseeeeea@gmail.com>
2022-11-01 18:37:16 +0800

    ”blog“

---
+++ Gemfile
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+source "https://rubygems.org"
+gem "minima", "~> 2.5"
+gem "jekyll", "~> 4.3.1"
+#gem "kramdown-parser-gfm"
---
+++ js/github_date.js
@@ -0,0 +1,15 @@
+const gh_token1 = "gh";
+const gh_token2 = "p_T0njguBA73rA4";
+const gh_token3 = "A6ELKdgdXlX5gkv0a0cvprG";
+
+let headers = new Headers();
+headers.append('Authorization', `token ${gh_token1 + gh_token2 + gh_token3}`);
+
+fetch("https://api.github.com/repos/donghoon-io/donghoon-website/branches/main", {method:'GET', headers: headers})
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+  let date = new Date(data.commit.commit.committer.date);
+  var dateString = moment(date).format('MMM DD, YYYY');
+  document.getElementById("date").innerText = dateString;
+});
